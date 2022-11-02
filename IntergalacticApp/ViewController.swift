ef0a5c3f809ec6c820c912ecd32bba5361947ced
Jakob Huber <93046@psdschools.org>
2022-11-02 09:36:29 -0600

    added background func

--- IntergalacticApp/ViewController.swift
+++ IntergalacticApp/ViewController.swift
@@ -12,8 +12,28 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        assignbackground()
     }
+    func assignbackground()
+    {
+            let background = UIImage(named: "SwiftImage")
 
-
+            var imageView : UIImageView!
+            imageView = UIImageView(frame: view.bounds)
+            imageView.contentMode =  UIView.ContentMode.scaleAspectFill
+            imageView.clipsToBounds = true
+            imageView.image = background
+            imageView.center = view.center
+            view.addSubview(imageView)
+            self.view.sendSubviewToBack(imageView)
+    }
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
