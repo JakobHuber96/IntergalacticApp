50d092ff0d8f1efa9eaf1048f9a94f8cff4e4dc0
Jakob Huber <93046@psdschools.org>
2022-11-03 10:01:18 -0600

    Functional, Randomize button is not working

--- IntergalacticApp/Secondview.swift
+++ IntergalacticApp/Secondview.swift
@@ -17,16 +17,28 @@ class Secondview: UIViewController {
     
    
     
-    var starsBackground = ""
+    var starsBackground = UIImage(named: "")
     var segueDecider = ""
     
     let redDecider = 0
     let blueDecider = 0
     
-  
+  var backGround = ""
+  var redDwarf1 = UIImage(named: "redDwarf1")
+    var redDwarf2 = UIImage(named: "redDwarf2")
+    var redDwarf3 = UIImage(named: "redDwarf3")
+    var blueDwarf1 = UIImage(named: "blueDwarf1")
+    var blueDwarf2 = UIImage(named: "blueDwarf2")
+    var blueDwarf3 = UIImage(named: "blueDwarf3")
+    
+    
+    var redDwarf11 = ""
+    var redDwarf22 = ""
+    var redDwarf33 = ""
+    
     
 
-    @IBOutlet weak var starImage: UIImageView!
+ 
     
     func assignbackground()
     {
@@ -34,67 +46,110 @@ class Secondview: UIViewController {
         if segueDecider == "blueDwarf" {
             
             if intOnethroughThree == 1 {
-        
-                starImage.image = UIImage(named: "blueDwarf1")
-        }
+                
+                starsBackground = blueDwarf1
+            }
             
             if intOnethroughThree == 2 {
-                starImage.image = UIImage(named: "blueDwarf2")
+                starsBackground = blueDwarf2
             }
-        
+            
             if intOnethroughThree == 3 {
-                starImage.image = UIImage(named: "blueDwarf3")
+                starsBackground = blueDwarf3
             }
             
         }
-            
-          
+        
+        
         if segueDecider == "redDwarf" {
             
             if intOnethroughThree == 1{
-                starImage.image = UIImage(named: "redDwarf1")
+                
+                starsBackground = redDwarf1
                 
             }
             
             if intOnethroughThree == 2{
                 
-                starImage.image = UIImage(named: "redDwarf2")
+                starsBackground = redDwarf2
+                
             }
             
             if intOnethroughThree == 3{
-                starImage.image = UIImage(named: "redDwarf3")
+                
+                starsBackground = redDwarf3
                 
             }
         }
         
         
-            let background = UIImage(named: "starsBackground")
-
-            var imageView : UIImageView!
-            imageView = UIImageView(frame: view.bounds)
-            imageView.contentMode =  UIView.ContentMode.scaleAspectFill
-            imageView.clipsToBounds = true
-            imageView.image = background
-            imageView.center = view.center
-            view.addSubview(imageView)
-            self.view.sendSubviewToBack(imageView)
-    }
-  
-   
-    @IBAction func buttontochangebackground(_ sender: Any) {
-        assignbackground()
-        let background = UIImage(named:"blueDwarf1 ")
+        let background = starsBackground
         
+        var imageView : UIImageView!
+        imageView = UIImageView(frame: view.bounds)
+        imageView.contentMode =  UIView.ContentMode.scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = background
+        imageView.center = view.center
+        view.addSubview(imageView)
+        self.view.sendSubviewToBack(imageView)
         
     }
-    
-    
-    @IBAction func randomize(_ sender: Any) {
+        
+        @IBAction func buttontochangebackground(_ sender: Any) {
+            let randomizerFunc = Int.random(in: 1...3)
+            
+            if segueDecider == "blueDwarf"{
+                if randomizerFunc == 1 {
+                    
+                    starsBackground = blueDwarf1
+                }
+                
+                if randomizerFunc == 2 {
+                    starsBackground = blueDwarf2
+                }
+                
+                if randomizerFunc == 3 {
+                    starsBackground = blueDwarf3
+                }
+                
+                
+                
+            }
+            
+            if segueDecider == "redDwarf"{
+                if randomizerFunc == 1{
+                    
+                    starsBackground = redDwarf1
+                    
+                }
+                
+                if randomizerFunc == 2{
+                    
+                    starsBackground = redDwarf2
+                    
+                }
+                
+                if randomizerFunc == 3{
+                    
+                    starsBackground = redDwarf3
+                    
+                }
+                
+                
+                
+                
+                
+            }
+            
+            
+      
+            
+            
+        }
         
         
         
-    }
-    
     
     
     
